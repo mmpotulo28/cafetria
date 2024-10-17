f9efe09ee5fcd34d5b1118d6a7bc071af0e7a6ae
mpotulom28 <mpotulom28@gmail.com>
2024-10-18 01:01:25 +0200

    Refactor db.ts to handle unexpected errors on idle client

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,4 +4,9 @@ const pool = new Pool({
 	connectionString: process.env.POSTGRES_STRING,
 });
 
+pool.on("error", (err) => {
+	console.error("Unexpected error on idle client", err);
+	process.exit(-1);
+});
+
 export default pool;
